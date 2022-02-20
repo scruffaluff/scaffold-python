efcbd123129748bccf6bee784e4820c8d8721f07
Macklan Weinstein <macklan.weinstein@gmail.com>
2022-02-20 11:04:40 -0800

    :white_check_mark: Disable MkDocs testing for Python 3.10

--- tests/__init__.py
+++ tests/__init__.py
@@ -122,8 +122,11 @@ def test_invalid_context(context: Dict[str, Any], cookies: Cookies) -> None:
 
 
 @pytest.mark.skipif(
-    sys.platform == "win32",
-    reason="Poetry install command hits permission errors for temporary paths.",
+    sys.platform == "win32" or sys.version_info >= (3, 10),
+    reason=(
+        "Poetry install command hits permission errors for temporary paths"
+        " and MkDocs does not yet work on Python 3.10."
+    ),
 )
 def test_mkdocs_build(cookies: Cookies) -> None:
     """Mkdocs must be able to build documentation for baked project."""
