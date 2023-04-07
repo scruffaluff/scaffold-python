47a5212e0c59bc0e11e04ecfb09eff446b434dc5
Macklan Weinstein <macklan.weinstein@gmail.com>
2023-04-07 06:22:54 -0700

    :fire: Simplify test directory layout

--- {{cookiecutter.project_name}}/tests/__init__.py
+++ {{cookiecutter.project_name}}/tests/__init__.py
@@ -6,30 +6,9 @@ import pathlib
 import toml
 
 import {{ cookiecutter.project_slug }}
-from tests import util
 
 
 REPO_PATH = pathlib.Path(__file__).parents[1]
-
-
-def test_matching_structure() -> None:
-    """Test directories match structure of src directory."""
-    src_path = REPO_PATH / "src/{{ cookiecutter.project_slug }}"
-    test_path = REPO_PATH / "tests"
-
-    error_msg = "Noe corresponding source code file {} for test file {}."
-
-    for dir_name in ["integration", "unit"]:
-        test_dir = test_path / dir_name
-        for test_file in util.find_test_files(test_dir):
-            src_file = src_path / util.remove_name_prefix(test_file, "test_")
-
-            rel_src_file = src_file.relative_to(src_path)
-            rel_test_file = (test_dir / test_file).relative_to(REPO_PATH)
-
-            assert src_file.exists(), error_msg.format(
-                rel_src_file, rel_test_file
-            )
 
 
 def test_version() -> None:
